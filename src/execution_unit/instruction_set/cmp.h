1b9168bc50c11c9cd430b289f01eb66ba2bf65ea
Ngenda Henry <ngendahk@gmail.com>
2022-09-07 11:40:19 +0300

    Add cmp instruction

---
+++ src/execution_unit/instruction_set/cmp.h
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2022 Ngenda Henry
+//
+// For the license information refer to LICENSE.
+
+#ifndef _CMP_H_
+#define _CMP_H_
+
+#include "types.h"
+
+struct CMP : MicroOp {};
+
+#endif // _CMP_H_
