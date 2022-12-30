ee4e4a050ba74b1ce8ef0384431d5505b77f93e8
Ngenda Henry <ngendahk@gmail.com>
2022-12-30 12:06:18 +0300

    Add instruction code map to instruction set

--- src/execution_unit/instruction_set/instruction_set.h
+++ src/execution_unit/instruction_set/instruction_set.h
@@ -8,6 +8,34 @@
 
 #include "types.h"
 #include <list>
+#include <array>
+#include <istream>
+
+struct InstructionCode {
+  uint8_t _opcode;
+  uint8_t _mode;
+  std::string _memonic;
+  std::array<std::string, 2> _arguments;
+
+  InstructionCode() : _opcode(0), _mode(0) {}
+
+  InstructionCode(uint8_t opcode) : _opcode(opcode), _mode(0) {}
+
+  InstructionCode(uint8_t opcode, std::string memonic)
+      : _opcode(opcode), _memonic(memonic) {}
+
+  InstructionCode(uint8_t opcode, std::string memonic, std::string larg)
+      : _opcode(opcode), _memonic(memonic), _arguments({larg, std::string()}) {}
+
+  InstructionCode(uint8_t opcode, std::string memonic, std::string larg,
+                  std::string rarg)
+      : _opcode(opcode), _memonic(memonic), _arguments({larg, rarg}) {}
+};
+
+struct InstructionCodeReader {
+  InstructionCode _opcode;
+  std::istream _instruction_buff;
+};
 
 struct Comparator {
   bool operator()(const MicroOp::Key &lhs, const MicroOp::Key &rhs) const {
@@ -33,6 +61,7 @@ struct InstructionSet {
 
 protected:
   std::list<std::pair<MicroOp::Key, create_func_t>> instructions;
+  std::map<uint8_t , InstructionCode> _instruction_code_map;
 };
 
 #endif // _INSTRUCTION_SET_H_
