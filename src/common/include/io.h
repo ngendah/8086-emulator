4be3ea37ca45286dcaa0e547099c859f1fcc0ea8
Ngenda Henry <ngendahk@gmail.com>
2023-05-12 13:40:33 +0300

    Add io helper methods

--- src/common/include/io.h
+++ src/common/include/io.h
@@ -6,7 +6,7 @@
 #ifndef _IO_H_
 #define _IO_H_
 
-#include <cstdint>
+#include "types.h"
 
 struct IO {
   IO() = default;
@@ -28,6 +28,50 @@ struct IO {
   virtual uint8_t read_hi() const = 0;
 
   virtual uint8_t read_lo() const = 0;
+
+  struct u16 {
+    u16(const u16 &val) : _parent(val._parent){};
+
+    void write(uint16_t val) { _parent->write(val); }
+
+    void write(u16_t val) { _parent->write(val.word); }
+
+    void write_hi(uint8_t val) { _parent->write_hi(val); }
+
+    void write_lo(uint8_t val) { _parent->write_lo(val); }
+
+    uint16_t read() const { return _parent->read(); }
+
+    uint8_t read_hi() const { return _parent->read_hi(); }
+
+    uint8_t read_lo() const { return _parent->read_lo(); }
+
+    u16_t read_u16() const { return u16_t(_parent->read()); }
+
+    friend struct IO;
+
+  private:
+    u16(IO *parent) : _parent(parent) {}
+    IO *_parent;
+  };
+
+  u16 to_u16() { return u16(dynamic_cast<IO *>(this)); }
+
+  struct u8 {
+    u8(const u8 &val) : _parent(val._parent){};
+
+    void write(uint8_t val) { _parent->write(val); }
+
+    uint8_t read() const { return _parent->read(); }
+
+    friend struct IO;
+
+  private:
+    u8(IO *parent) : _parent(parent) {}
+    IO *_parent;
+  };
+
+  u8 to_u8() { return u8(dynamic_cast<IO *>(this)); }
 };
 
 #endif // _IO_H_
--- src/cpu/execution_unit/instruction_set/core/include/bus_io.h
+++ src/cpu/execution_unit/instruction_set/core/include/bus_io.h
@@ -12,8 +12,14 @@
 #include <fmt/core.h>
 #include <iostream>
 
-struct BUSIO final : IO {
+/**
+ * @brief Latch the address for IO operations
+ *
+ */
+struct BUSIO final : IO { // TODO rename to BusAddressLatch
   explicit BUSIO(BUS *bus) : _bus(bus) {}
+
+  BUSIO(bus_ptr_t bus, const Address &address) : _bus(bus), _address(address){};
 
   BUSIO(const BUSIO &rhs) : _bus(rhs._bus), _address(rhs._address) {}
 
@@ -47,36 +53,31 @@ struct BUSIO final : IO {
   }
 
   void write(const uint8_t val) override {
-    Bytes bytes((uint8_t *)&val, sizeof(uint8_t));
+    Bytes bytes(const_cast<uint8_t *>(&val), sizeof(uint8_t));
     _bus->write(&_address, bytes);
   }
 
   uint16_t read() const override {
     Bytes bytes = _bus->read(&_address, sizeof(uint16_t));
-    uint16_t word = 0;
-    std::memcpy(&word, bytes._bytes, bytes._size);
+    uint16_t word = *reinterpret_cast<uint16_t *>(bytes._bytes);
     return word;
   }
 
   uint8_t read_byte() const override {
     Bytes bytes = _bus->read(&_address, sizeof(uint8_t));
-    uint8_t byte = 0;
-    std::memcpy(&byte, bytes._bytes, bytes._size);
+    uint8_t byte = *reinterpret_cast<uint8_t *>(bytes._bytes);
     return byte;
   }
 
   uint8_t read_hi() const override {
     Bytes bytes = _bus->read(&_address, sizeof(uint16_t));
-    uint16_t word = 0;
-    std::memcpy(&word, bytes._bytes, sizeof(uint16_t));
-    return ((word >> 8) & 0x107);
+    uint16_t word = *reinterpret_cast<uint16_t *>(bytes._bytes);
+    return ((word >> 8) & 0xff);
   }
 
   uint8_t read_lo() const override {
     Bytes bytes = _bus->read(&_address, sizeof(uint8_t));
-    uint16_t byte = 0;
-    std::memcpy(&byte, bytes._bytes, sizeof(uint8_t));
-    return byte;
+    return *reinterpret_cast<uint8_t *>(bytes._bytes);
   }
 
   friend std::ostream &operator<<(std::ostream &ostream, const BUSIO &t) {
