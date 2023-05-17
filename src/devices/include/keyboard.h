3e17d4d0a1262efd6aefd4193b782ad26c08ac26
Ngenda Henry <ngendahk@gmail.com>
2023-05-17 11:41:17 +0300

    Remove impl not required at the moment

--- src/devices/include/keyboard.h
+++ src/devices/include/keyboard.h
@@ -11,24 +11,14 @@
 
 struct KeyBoard final : Device {
   struct _Port final : Port {
-    void write_hi(uint8_t val) override { Port::write_hi(val); }
-    void write_lo(uint8_t val) override { Port::write_lo(val); }
-    void write(uint8_t val) override { Port::write(val); }
-    void write(uint16_t val) override { Port::write(val); }
-    uint16_t read() const override { return Port::read(); }
-    uint8_t read_byte() const override { return Port::read_byte(); }
-    uint8_t read_hi() const override { return Port::read_hi(); }
-    uint8_t read_lo() const override { return Port::read_lo(); }
     friend struct KeyBoard;
 
   private:
     _Port(KeyBoard *keyboard) : _keyboard(keyboard) {}
-
-    uint8_t _control_register{};
     KeyBoard *_keyboard;
   };
 
-  uint16_t _port_number = 0x61;
+  const uint16_t _port_number = 0x61;
   _Port _port;
   InterruptHandler *_interrupt_handler{};
 
